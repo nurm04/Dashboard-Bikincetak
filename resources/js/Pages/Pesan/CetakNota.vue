589aa5fa14ba166184a0c351126e0d1dc20cc986
nurm04 <nurm0892709@gmail.com>
2026-08-07 00:33:46 +0700

    a

--- resources/js/Pages/Pesan/CetakNota.vue
+++ resources/js/Pages/Pesan/CetakNota.vue
@@ -173,7 +173,7 @@ onMounted(() => {
             <!-- Header Toko & Tujuan -->
             <div class="flex items-start justify-between pb-4 border-b border-black">
                 <div>
-                    <h1 class="text-2xl font-black tracking-tight text-blue-950">BIKIN Cetak</h1>
+                    <h1 class="text-2xl font-black tracking-tight text-blue-950">BIKIN CETAK</h1>
                     <p class="text-[10px] italic text-gray-700">Digital Printing, Offset, Merchandise</p>
                     <p class="text-[9px] mt-1.5 text-gray-800 leading-tight">
                         WA : 081213139490 | Email : order@bikincetak.co.id <br>
